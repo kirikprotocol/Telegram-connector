cfbcf8a3c594912f2816bd84e2e159695bf16815
chukanov <chukanov@eyeline.mobi>
2016-02-19 18:09:54 +0600

    Modified telegram link adaptor

--- src/com/eyelinecom/whoisd/sads2/telegram/adaptors/LinkToTelegramAdaptor.java
+++ src/com/eyelinecom/whoisd/sads2/telegram/adaptors/LinkToTelegramAdaptor.java
@@ -7,6 +7,7 @@ import com.eyelinecom.whoisd.sads2.common.SADSInitUtils;
 import com.eyelinecom.whoisd.sads2.common.SADSLogger;
 import com.eyelinecom.whoisd.sads2.content.ContentResponse;
 import com.eyelinecom.whoisd.sads2.content.ContentResponseUtils;
+import com.eyelinecom.whoisd.sads2.executors.connector.SADSInitializer;
 import com.eyelinecom.whoisd.sads2.telegram.registry.WebHookConfigListener;
 import com.eyelinecom.whoisd.sads2.telegram.resource.TelegramApi;
 import org.apache.commons.lang.RandomStringUtils;
@@ -18,6 +19,7 @@ import java.util.Properties;
  * Created by jeck on 18/02/16
  */
 public class LinkToTelegramAdaptor extends URLAdaptor {
+    private static final String PREFIX_TELEGRAM = "protocol://telegram";
     public static final String PERS_VAR_TELEGRAM_HASH_PREFIX = "tg-hash-";
     private String telegramAddress;
     private PersonalizationManager personalization;
@@ -27,14 +29,21 @@ public class LinkToTelegramAdaptor extends URLAdaptor {
     @Override
     protected String adaptLink(String linkUri, ContentResponse content) throws Exception {
         String service = content.getServiceScenario().getId();
-        String token = content.getServiceScenario().getAttributes().getProperty(WebHookConfigListener.CONF_TOKEN);
-        if (linkUri.startsWith("protocol://telegram")) {
+        if (linkUri.startsWith(PREFIX_TELEGRAM)) {
             String subscriber = ContentResponseUtils.getAbonent(content);
             Log log = SADSLogger.getLogger(service, subscriber, this.getClass());
             String hash = RandomStringUtils.randomAlphanumeric(hashLength);
             String var = PERS_VAR_TELEGRAM_HASH_PREFIX+hash;
             personalization.set(var, subscriber);
-            String link = telegramAddress+api.getMe(token).getUserName()+"?start="+hash;
+            String token;
+            if (linkUri.length() > PREFIX_TELEGRAM.length()+1) {
+                service = linkUri.substring(PREFIX_TELEGRAM.length()+1);
+                token = SADSInitializer.getServiceRegistry().getProperties(service).getProperty(WebHookConfigListener.CONF_TOKEN);
+            } else {
+                token = content.getServiceScenario().getAttributes().getProperty(WebHookConfigListener.CONF_TOKEN);
+            }
+            String botUsername  = api.getMe(token).getUserName();
+            String link = telegramAddress+botUsername+"?start="+hash;
             //String link = telegramAddress+"UruruBot?start="+hash;
             if (log.isInfoEnabled()) {
                 log.info("Saved to personalization '"+var+"' value '"+subscriber+"', result link: "+link);
