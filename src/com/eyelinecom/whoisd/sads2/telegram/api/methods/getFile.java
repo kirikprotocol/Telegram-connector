4459cf955e9fae465a077f5d384726610e2edc1f
chukanov <chukanov@eyeline.mobi>
2016-03-31 17:31:14 +0600

    added MO video, contacts, files, location

--- src/com/eyelinecom/whoisd/sads2/telegram/api/methods/getFile.java
+++ src/com/eyelinecom/whoisd/sads2/telegram/api/methods/getFile.java
@@ -1,7 +1,27 @@
 package com.eyelinecom.whoisd.sads2.telegram.api.methods;
 
+import com.eyelinecom.whoisd.sads2.telegram.api.types.File;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by jeck on 30/03/16
  */
-public class getFile {
+public class getFile extends ApiMethod<getFile, File> {
+    @JsonProperty(value = "file_id", required = true)
+    private String fileId;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public getFile() {
+    }
+
+    public getFile(String fileId) {
+        this.fileId = fileId;
+    }
 }
