289061e043b800cc3191d9a832ac406834ddb681
chukanov <chukanov@eyeline.mobi>
2016-04-08 21:18:59 +0600

    fixed msisdn redirect

--- src/com/eyelinecom/whoisd/sads2/telegram/interceptors/MsisdnRedirectInterceptor.java
+++ src/com/eyelinecom/whoisd/sads2/telegram/interceptors/MsisdnRedirectInterceptor.java
@@ -8,6 +8,7 @@ import com.eyelinecom.whoisd.sads2.common.SADSLogger;
 import com.eyelinecom.whoisd.sads2.connector.SADSRequest;
 import com.eyelinecom.whoisd.sads2.exception.InterceptionException;
 import com.eyelinecom.whoisd.sads2.interceptor.BlankInterceptor;
+import com.eyelinecom.whoisd.sads2.telegram.connector.ExtendedSadsRequest;
 import com.eyelinecom.whoisd.sads2.telegram.registry.WebHookConfigListener;
 import com.eyelinecom.whoisd.sads2.wstorage.profile.Profile;
 import com.eyelinecom.whoisd.sads2.wstorage.profile.ProfileStorage;
@@ -17,6 +18,7 @@ import java.util.Properties;
 
 import static com.eyelinecom.whoisd.sads2.wstorage.profile.QueryRestrictions.property;
 import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.apache.commons.lang.StringUtils.remove;
 
 public class MsisdnRedirectInterceptor extends BlankInterceptor implements Initable {
 
@@ -28,17 +30,14 @@ public class MsisdnRedirectInterceptor extends BlankInterceptor implements Inita
   public void onRequest(SADSRequest request, RequestDispatcher dispatcher) throws InterceptionException {
     if (InitUtils.getBoolean(CONF_MSISDN_CONFIRMATION_FORCED, false, request.getServiceScenario().getAttributes())) {
       final Log log = SADSLogger.getLogger(request.getServiceId(), getClass());
-      if (!isEnabled(request)) {
+      if (!isEnabled(request) || !(request instanceof ExtendedSadsRequest)) {
         return;
       }
       try {
-        final String chatId = request.getAbonent();
-        final String token =
-            request.getServiceScenario().getAttributes().getProperty(WebHookConfigListener.CONF_TOKEN);
-
-        final String msisdn = getMsisdn(chatId, token);
+        Profile profile = ((ExtendedSadsRequest) request).getProfile();
+        final String msisdn = getMsisdn(profile);
         if (log.isDebugEnabled()) {
-          log.debug("Processing chatId = [" + chatId + "], stored msisdn = [" + msisdn + "]");
+          log.debug("Processing wnumber = [" + profile.getWnumber() + "], stored msisdn = [" + msisdn + "]");
         }
         if (isBlank(msisdn)) {
           String redirectURL = request.getServiceScenario().getAttributes().getProperty("msisdn-redirect-uri");
@@ -57,16 +56,10 @@ public class MsisdnRedirectInterceptor extends BlankInterceptor implements Inita
     return InitUtils.getBoolean(CONF_MSISDN_CONFIRMATION_FORCED, false, request.getServiceScenario().getAttributes());
   }
 
-  private String getMsisdn(String chatId, String token) {
-    final Profile profile = profileStorage
-        .query()
-        .where(property("telegram-chats", token).eq(chatId))
-        .get();
-
+  private String getMsisdn(Profile profile) {
     if (profile == null) {
       return null;
     }
-
     final Profile.Property msisdn = profile
         .query()
         .property("mobile", "msisdn")
