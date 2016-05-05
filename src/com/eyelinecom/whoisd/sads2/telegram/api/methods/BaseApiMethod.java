3f9f153e29bd7d6232d67634aa43b8eaf3741dcf
andy <andy@ussd.mobi>
2016-05-05 19:53:38 +0600

    1. Skype module introduced 2. Skype API v2 model

--- src/com/eyelinecom/whoisd/sads2/telegram/api/methods/BaseApiMethod.java
+++ src/com/eyelinecom/whoisd/sads2/telegram/api/methods/BaseApiMethod.java
@@ -6,25 +6,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.commons.lang.StringUtils;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-
 public abstract class BaseApiMethod<Self extends BaseApiMethod, Response extends ApiType> {
 
   Class<Self> methodClass;
   Class<Response> responseClass;
-
-  static <X> Class<X> getEntityClass(Class<?> self, int argNo) {
-    Type generic = self.getGenericSuperclass();
-    if (generic instanceof Class) {
-      generic = ((Class) generic).getGenericSuperclass();
-    }
-
-    final ParameterizedType genericSuperclass =
-        (ParameterizedType) generic;
-    //noinspection unchecked
-    return (Class<X>) genericSuperclass.getActualTypeArguments()[argNo];
-  }
 
   /**
    * @return Method name, as should be passed in {@code method} parameter of a WebHook response.
